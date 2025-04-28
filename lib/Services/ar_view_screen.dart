55c5d6068e15045d55ef60fb87fda0aeaf5598b2
Zheheng <zheheng@33digitec.com>
2025-04-28 12:58:52 +0800

    Fix

--- lib/Services/ar_view_screen.dart
+++ lib/Services/ar_view_screen.dart
@@ -1,28 +1,21 @@
-import 'package:flutter/material.dart';
 import 'package:arcore_flutter_plugin/arcore_flutter_plugin.dart';
+import 'package:flutter/material.dart';
 import 'package:vector_math/vector_math_64.dart' as vector;
 
 class ARViewScreen extends StatefulWidget {
   const ARViewScreen({Key? key}) : super(key: key);
 
   @override
-  State<ARViewScreen> createState() => _ARViewScreenState();
+  _ARViewScreenState createState() => _ARViewScreenState();
 }
 
 class _ARViewScreenState extends State<ARViewScreen> {
   late ArCoreController arCoreController;
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Simple AR View'),
-      ),
-      body: ArCoreView(
-        onArCoreViewCreated: _onArCoreViewCreated,
-        enableTapRecognizer: true,
-      ),
-    );
+  void dispose() {
+    arCoreController.dispose();
+    super.dispose();
   }
 
   void _onArCoreViewCreated(ArCoreController controller) {
@@ -31,20 +24,23 @@ class _ARViewScreenState extends State<ARViewScreen> {
   }
 
   void _addSphere() {
-    final material = ArCoreMaterial(color: Colors.blueAccent);
+    final material = ArCoreMaterial(color: Colors.blue);
     final sphere = ArCoreSphere(materials: [material], radius: 0.1);
-
     final node = ArCoreNode(
       shape: sphere,
       position: vector.Vector3(0, 0, -1.0),
     );
-
     arCoreController.addArCoreNode(node);
   }
 
   @override
-  void dispose() {
-    arCoreController.dispose();
-    super.dispose();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('AR View')),
+      body: ArCoreView(
+        onArCoreViewCreated: _onArCoreViewCreated,
+        enableTapRecognizer: true,
+      ),
+    );
   }
 }
